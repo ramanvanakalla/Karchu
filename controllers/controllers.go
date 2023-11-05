ff874bdd28a25864068c7bacc8895825ae2e1aaa
Raman Vanakalla <ramanvanakalla123@gmail.com>
2023-11-05 19:16:17 +0530

    remove usercontrollers file, use single file for all kind of request

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,25 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"Karchu/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(ctx *gin.Context) {
+	var user models.User
+	ctx.Bind(&user)
+	code, msg := user.CreateUser()
+	if code == "SUCCESS" {
+		ctx.JSON(200, createSuccessResponse(code, msg))
+	} else if code == "INVALID_USER_DETAILS" {
+		ctx.JSON(400, createErrorResponse(code, msg))
+	} else if code == "EMAIL_ALREADY_EXISTS" {
+		ctx.JSON(409, createErrorResponse(code, msg))
+	} else { //code is "DB_CONNECTIVITY_ISSUE" or "DB_INSERT_ERROR"  or anything {
+		ctx.JSON(500, createErrorResponse(code, msg))
+	}
+}
 
 func GetCategories(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
