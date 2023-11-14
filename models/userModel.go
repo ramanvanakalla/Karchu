b4730fece4837420b7ab0a49ce9f67c483e7bbaa
Raman Vanakalla <ramanvanakalla123@gmail.com>
2023-11-14 22:23:30 +0530

    change the digit parsing

--- models/userModel.go
+++ models/userModel.go
@@ -124,7 +124,7 @@ func (user *User) GetCategories() ([]string, error) {
 }
 
 func (transaction *Transaction) toString() string {
-	return transaction.Category + string(transaction.Amount) + transaction.SplitTag + transaction.Description
+	return transaction.Category + fmt.Sprintf("%d", transaction.Amount) + transaction.SplitTag + transaction.Description
 }
 
 func (user *User) GetLastNTransactions(lastNDays int) ([]string, error) {
