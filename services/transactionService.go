8761b16c597214e132fafa447580555ea4a583ad
Raman Vanakalla <ramanvanakalla123@gmail.com>
2023-12-01 23:31:00 +0530

    change parse string

--- services/transactionService.go
+++ services/transactionService.go
@@ -39,7 +39,7 @@ func DeleteTransactionFromTransString(TransString string, userId uint) (uint, *e
 func StringToTransaction(input string) (*models.Transaction, error) {
 	var transaction models.Transaction
 	fmt.Println(input)
-	_, err := fmt.Sscanf(input, "Id: %d|Amount: %d|category: %s|splitTag: %s|Desc: %s",
+	_, err := fmt.Sscanf(input, "Id: %d|Amount: %d|category: %[^\n]|splitTag: %[^\n]|Desc: %[^\n]",
 		&transaction.ID, &transaction.Amount, &transaction.Category, &transaction.SplitTag, &transaction.Description)
 
 	if err != nil {
