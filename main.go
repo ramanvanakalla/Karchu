cccfcba3ea3e63e80dd336c417ad998587839ab1
Raman Vanakalla <ramanvanakalla123@gmail.com>
2023-12-15 23:58:03 +0530

    change the base url

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func init() {
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:3000
+// @host      https://karchu.onrender.com/
 // @BasePath  /v1
 
 // @securityDefinitions.basic  BasicAuth
