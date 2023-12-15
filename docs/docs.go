236a6fef4d7993c17a2134ce996fdc36b114e929
Raman Vanakalla <ramanvanakalla123@gmail.com>
2023-12-16 01:15:03 +0530

    add swag docs for routes

--- docs/docs.go
+++ docs/docs.go
@@ -145,7 +145,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://karchu.onrender.com",
+	Host:             "karchu.onrender.com",
 	BasePath:         "/v1",
 	Schemes:          []string{},
 	Title:            "Karchu API",
