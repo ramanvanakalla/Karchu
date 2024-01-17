e0ea49825e6a7466274a5a4bbde8a60dbfbfd5a0
ramanvanakalla <ramanvanakalla123@gmail.com>
2024-01-17 19:30:51 +0530

    change the index consider deleted at

--- models/splitTransaction.go
+++ models/splitTransaction.go
@@ -6,8 +6,9 @@ import (
 
 type SplitTransaction struct {
 	gorm.Model
-	SourceTransactionId  uint `gorm:"uniqueIndex:idx_transaction_friend;index;foreignkey:SourceTransactionId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	SourceTransactionId  uint `gorm:"uniqueIndex:idx_transaction_friend_deleted;index;foreignkey:SourceTransactionId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Amount               int
-	FriendId             uint  `gorm:"uniqueIndex:idx_transaction_friend;index;foreignkey:FriendId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	SettledTransactionId *uint `gorm:"unique;foreignKey:SettledTransactionId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	FriendId             uint           `gorm:"uniqueIndex:idx_transaction_friend_deleted;index;foreignkey:FriendId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	SettledTransactionId *uint          `gorm:"unique;foreignKey:SettledTransactionId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	DeletedAt            gorm.DeletedAt `gorm:"uniqueIndex:idx_transaction_friend_deleted;index"`
 }
